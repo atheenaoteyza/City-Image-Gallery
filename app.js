356e687f638031a4cd8f8dfcb5ce6d20eed77b0a
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-08-06 02:32:02 +0800

    no commit message

--- app.js
+++ app.js
@@ -7,6 +7,39 @@ async function fetchMessage(url) {
   return results;
 }
 
+function getUrl(input) {
+  const url = `https://api.unsplash.com/search/photos?page=1&query=${input}&client_id=TWvM_zLxG9ar3m65o0pYxe_7lbNpXHpmhsKc_eR4Qbs`;
+  return url;
+}
+
+function getImgSrc(urlsArray) {
+  const { urls } = urlsArray;
+  return urls.raw;
+}
+
+function getArrayOfImgUrl(fetchedResult) {
+  let newArray = [];
+  fetchedResult.forEach((element) => {
+    newArray.push(getImgSrc(element));
+  });
+  return newArray;
+}
+
+const fetchedSample = await fetchMessage(
+  "https://api.unsplash.com/search/photos?page=1&query=new%20york%201800&client_id=TWvM_zLxG9ar3m65o0pYxe_7lbNpXHpmhsKc_eR4Qbs"
+);
+const sampleArrayOfRawImgs = getArrayOfImgUrl(fetchedSample);
+
+function switchBg() {
+  const flexItems = document.querySelectorAll(".flex-items");
+  const arrayOfImgs = sampleArrayOfRawImgs;
+  for (let i = 0; i < flexItems.length; i++) {
+    if (i < arrayOfImgs.length) {
+      flexItems[i].style.backgroundImage = `url(${arrayOfImgs[i]})`;
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const flexItems = document.querySelectorAll(".flex-items");
 
